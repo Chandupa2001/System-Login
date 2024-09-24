8a2cba1d0ffc2ae265a797500b725be73023521e
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-09-24 23:02:00 +0530

    Created login page frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Login from './pages/login/login'
 
 function App() {
   return (
     <>
       <div className='app'>
       <Routes>
+        <Route path='/' element={<Login />} />
       </Routes>
     </div>
     </>
