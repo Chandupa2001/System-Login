8241cd25d13b69944ea42a6c90499c7d08bbb375
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-09-24 19:54:44 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <div className='app'>
+      <Routes>
+      </Routes>
+    </div>
+    </>
+  )
+}
+
+export default App
