eefb9e56951975a07527481ac50e479509ec7ceb
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-09-24 23:16:29 +0530

    Created signup page frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/login/login'
+import SignUp from './pages/SignUp/SignUp'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className='app'>
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
     </div>
     </>
