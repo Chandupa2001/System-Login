6d75887e0f51eeea8138cffea3c2ffb851246e20
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-09-25 22:38:45 +0530

    updated database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async() => {
     try {
-      await mongoose.connect('mongodb://chanduparanawaka2001:chane@cluster0-shard-00-00.zydsz8d.mongodb.net:27017,cluster0-shard-00-01.zydsz8d.mongodb.net:27017,cluster0-shard-00-02.zydsz8d.mongodb.net:27017/login-system?ssl=true&replicaSet=atlas-fgwxyz-shard-0&authSource=admin&retryWrites=true&w=majority');
+      await mongoose.connect('mongodb://localhost:27017/login-system');
       console.log("DB Connected");
     } catch (err) {
       console.error("Connection error", err);
