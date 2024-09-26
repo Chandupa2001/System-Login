7ea8dd1e86e41ef8af380177ed7d912ef415db74
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-09-26 13:31:29 +0530

    added password toggle button

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,63 +1,95 @@
-import React, { useState } from 'react'
-import './Login.css'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState } from "react";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { FaEyeSlash, FaRegEye } from "react-icons/fa";
 
 const Login = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [token, setToken] = useState("");
+  const [hidePass, setHidePass] = useState(true);
 
-    const [data, setData] = useState({
-        email: "",
-        password: ""
-    })
-    const [token, setToken] = useState("")
+  const navigation = useNavigate();
 
-    const navigation = useNavigate();
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData({ ...data, [name]: value });
+  };
 
-    const onChangeHandler = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setData({...data, [name]: value})
+  const onLogin = async (event) => {
+    event.preventDefault();
+    let url = "http://localhost:5000/api/user/login";
+
+    try {
+      const response = await axios.post(url, data);
+      if (response.data.success) {
+        setToken(response.data.success);
+        localStorage.setItem("token", response.data.token);
+        console.log("Logged In");
+      } else {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
     }
-
-    const onLogin = async () => {
-        event.preventDefault();
-        let url = "http://localhost:5000/api/user/login"
-
-        try {
-            const response = await axios.post(url,data)
-            if (response.data.success) {
-                setToken(response.data.success);
-                localStorage.setItem("token", response.data.token)
-                console.log("Logged In")
-            } else {
-                alert(response.data.message)
-            }
-        } catch (error) {
-            console.error(error)
-        }
-    }
+  };
 
   return (
-    <div className='login'>
-        <div className="login-container">
-            <h2>Login</h2>
-            <form onSubmit={onLogin}>
-                <div className="input-container">
-                    <label htmlFor="email">Email</label><br/>
-                    <input type="email" onChange={onChangeHandler} name="email" placeholder='Email Address' value={data.email} required />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="password">Password</label><br/>
-                    <input type="password" onChange={onChangeHandler} name="password" placeholder='Password' value={data.password} required />
-                </div>
-                <button type='submit'>Login</button>
-            </form>
-            <p className="signup-text">
-                Do not have an account? <span onClick={() => navigation('/signup')}>Sign Up</span>
-            </p>
-        </div>
-    </div>
-  )
-}
+    <div className="login">
+      <div className="login-container">
+        <h2>Login</h2>
+        <form onSubmit={onLogin}>
+          <div className="input-container">
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              type="email"
+              onChange={onChangeHandler}
+              name="email"
+              placeholder="Email Address"
+              value={data.email}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="password">Password</label>
+            <br />
+            <div className="input-icon-wrapper">
+              <input
+                type={hidePass ? "password" : "text"}
+                id="password"
+                onChange={onChangeHandler}
+                name="password"
+                placeholder="Password"
+                value={data.password}
+                required
+              />
+              {
+                hidePass ? <FaRegEye
+                className="password-icon"
+                onClick={() => setHidePass(!hidePass)}
+              /> : 
+              <FaEyeSlash
+                className="password-icon"
+                onClick={() => setHidePass(!hidePass)}
+              />
+              }
+            </div>
+          </div>
 
-export default Login
+          <button type="submit">Login</button>
+        </form>
+        <p className="signup-text">
+          Do not have an account?{" "}
+          <span onClick={() => navigation("/signup")}>Sign Up</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,73 +1,121 @@
-import React, { useState } from 'react'
-import './SignUp.css'
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios'
+import React, { useState } from "react";
+import "./SignUp.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { FaEyeSlash, FaRegEye } from "react-icons/fa";
 
 const SignUp = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phoneNumber: "",
+  });
+  const [token, setToken] = useState("");
+  const [hidePass, setHidePass] = useState(true);
 
-    const [data, setData] = useState({
-        name: "",
-        email: "",
-        password: "",
-        phoneNumber: ""
-    })
-    const [token, setToken] = useState("")
+  const navigation = useNavigate();
 
-    const navigation = useNavigate();
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
 
-    const onChangeHandler = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setData(data => ({...data,[name]:value}))
+  const onSignUp = async (event) => {
+    event.preventDefault();
+    let url = "http://localhost:5000/api/user/signup";
+
+    try {
+      const response = await axios.post(url, data);
+      if (response.data.success) {
+        setToken(response.data.token);
+        localStorage.setItem("token", response.data.token);
+        alert("Registered Successfully");
+      } else {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    const onSignUp = async (event) => {
-        event.preventDefault();
-        let url = "http://localhost:5000/api/user/signup"
-
-        try {
-            const response = await axios.post(url,data)
-            if (response.data.success) {
-                setToken(response.data.token);
-                localStorage.setItem("token", response.data.token);
-                alert("Registered Successfully")
-            } else {
-                alert(response.data.message);
-            }
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    return (
-        <div className='signup'>
-            <div className="signup-container">
-                <h2>Sign Up</h2>
-                <form onSubmit={onSignUp}>
-                    <div className="input-container">
-                        <label htmlFor="name">Name</label><br />
-                        <input type="text" onChange={onChangeHandler} name="name" value={data.name} placeholder='Name' required />
-                    </div>
-                    <div className="input-container">
-                        <label htmlFor="email">Email</label><br />
-                        <input type="email" onChange={onChangeHandler} name="email" value={data.email} placeholder='Email Address' required />
-                    </div>
-                    <div className="input-container">
-                        <label htmlFor="password">Password</label><br />
-                        <input type="password" onChange={onChangeHandler} name="password" value={data.password} placeholder='Password' required />
-                    </div>
-                    <div className="input-container">
-                        <label htmlFor="phoneNo">Phone Number</label><br />
-                        <input type="tel" onChange={onChangeHandler} name="phoneNumber" value={data.phoneNumber} placeholder='Phone Number' required />
-                    </div>
-                    <button type='submit'>Sign Up</button>
-                </form>
-                <p className="login-text">
-                    Already have an account? <span onClick={() => navigation('/')}>Login</span>
-                </p>
+  return (
+    <div className="signup">
+      <div className="signup-container">
+        <h2>Sign Up</h2>
+        <form onSubmit={onSignUp}>
+          <div className="input-container">
+            <label htmlFor="name">Name</label>
+            <br />
+            <input
+              type="text"
+              onChange={onChangeHandler}
+              name="name"
+              value={data.name}
+              placeholder="Name"
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              type="email"
+              onChange={onChangeHandler}
+              name="email"
+              value={data.email}
+              placeholder="Email Address"
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="password">Password</label>
+            <br />
+            <div className="input-icon-wrapper">
+              <input
+                type={hidePass ? "password" : "text"}
+                id="password"
+                onChange={onChangeHandler}
+                name="password"
+                placeholder="Password"
+                value={data.password}
+                required
+              />
+              {hidePass ? (
+                <FaRegEye
+                  className="password-icon"
+                  onClick={() => setHidePass(!hidePass)}
+                />
+              ) : (
+                <FaEyeSlash
+                  className="password-icon"
+                  onClick={() => setHidePass(!hidePass)}
+                />
+              )}
             </div>
-        </div>
-    )
-}
+          </div>
+          <div className="input-container">
+            <label htmlFor="phoneNo">Phone Number</label>
+            <br />
+            <input
+              type="tel"
+              onChange={onChangeHandler}
+              name="phoneNumber"
+              value={data.phoneNumber}
+              placeholder="Phone Number"
+              required
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+        <p className="login-text">
+          Already have an account?{" "}
+          <span onClick={() => navigation("/")}>Login</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
